37a51c40abf067059f8967c846ccaf721047d80d
deepthib <deepthibrndvnm@gmail.com>
2013-11-30 23:16:39 -0800

    updating import statements

--- dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/api/resources/AppointmentResource.java
+++ dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/api/resources/AppointmentResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.Response;
 
 import edu.sjsu.cmpe.projectdemo.domain.AllAppointments;
 import edu.sjsu.cmpe.projectdemo.views.AppointmentView;
+import edu.sjsu.cmpe.projectdemo.views.TimeView;
 
 
 @Path("login/donor/clinics/appointments/")
