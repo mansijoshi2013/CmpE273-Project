5a5ebee76017a6e9010570d7a448663fa3d84a9f
TejasviBojja <tejasvi.bojja@gmail.com>
2013-12-02 06:08:55 -0800

    search donor result view file

---
+++ dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/views/SearchDonorResultView.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe.projectdemo.views;
+
+import com.yammer.dropwizard.views.View;
+
+public class SearchDonorResultView extends View
+{
+	public SearchDonorResultView()
+	{
+		super("searchdonorresult.ftl");
+		
+	}
+
+}
