2c850bbaeaf91b06f3ed90f6ef53aa3ed9d35c8b
deepthib <deepthibrndvnm@gmail.com>
2013-12-04 13:22:12 -0800

    Update RootPath.java

--- dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/domain/RootPath.java
+++ dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/domain/RootPath.java
@@ -2,10 +2,10 @@ package edu.sjsu.cmpe.projectdemo.domain;
 
 public class RootPath {
 
-<<<<<<< HEAD
-=======
+
+
 //	private static String host = "50.18.202.70:";
->>>>>>> ed4ed789041682088341b68aefa7b6e8ee90edc0
+
 	private static String host = "localhost:";
 	private static String portNum = "15000";
 	public static String rootPath = host + portNum; 
