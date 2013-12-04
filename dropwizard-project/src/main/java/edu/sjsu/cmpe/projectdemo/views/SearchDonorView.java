f0e0742034082b073adf2f6a568da3eece7b41df
TejasviBojja <tejasvi.bojja@gmail.com>
2013-12-04 01:58:55 -0800

    SearchDonorView

--- dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/views/SearchDonorView.java
+++ dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/views/SearchDonorView.java
@@ -1,24 +1,13 @@
 package edu.sjsu.cmpe.projectdemo.views;
 
-import java.util.ArrayList;
-
 import com.yammer.dropwizard.views.View;
 
-import edu.sjsu.cmpe.projectdemo.domain.Donor;
-
-
-public class SearchDonorResultView extends View
+public class SearchDonorView extends View
 {
-	private ArrayList<Donor> donors = new ArrayList<Donor>();
-	
-	public SearchDonorResultView(ArrayList<Donor> donors)
+	public SearchDonorView()
 	{
-		super("/searchdonorresult.ftl");
-		this.donors=donors;
+		super("/searchdonor.ftl");
 		
 	}
-	
-	public ArrayList<Donor> getDonors(){
-		return this.donors;
-	}
+
 }
